1e72aee51f2387f4e3dd0924b9097c152b75037f
Lenka <janosova5@stud.uniza.sk>
2020-06-23 10:51:24 +0200

    Xcode project init.

---
+++ Cleverlogin/Cleverlogin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cleverlogin
+//
+//  Created by Lenka Janosova on 23/06/2020.
+//  Copyright © 2020 Lenka Janosova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
